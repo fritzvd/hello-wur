1de02f813035a2b221a32a9aca53af196b5114c2
fritzvd <f.vandeventer@gmail.com>
2016-01-05 16:33:00 +0100

    small template"

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def app():
-	return 'Hello'
+def hello():
+	template = 'Hello {}'
+	name = request.args.get('name', 'World')
+	return template.format(name)
+
+if __name__ == '__main__':
+	app.run(debug=True)

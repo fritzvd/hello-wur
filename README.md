e270fefa4156391a96423f7d1d2c6d0eb1195533
fritzvd <f.vandeventer@gmail.com>
2016-01-05 16:56:13 +0100

    distance

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # hello-wur
 Hello WURld
+
+Taken from this example: https://devcenter.heroku.com/articles/getting-started-with-python-o
+
+# Run
+```bash
+git clone https://github.com/fritzvd/hello-wur.git
+cd hello-wur
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+run locally with plain ole Python and pip
+```
+python main.py
+```
+
+Or free with/on heroku (Assuming you have heroku credentials). Install [heroku toolbelt](https://toolbelt.heroku.com/).
+```
+heroku login
+heroku create
+git push heroku master
+```
+
+### Hello *World*
+Runs here: http://mysterious-castle-6257.herokuapp.com/?name=fritz
+
+### Distance between points
+Runs here: http://mysterious-castle-6257.herokuapp.com/distance?from=53.3,4.4&to=51.4,3.6
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask import request
 
+from haversine import haversine
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +11,19 @@ def hello():
 	template = 'Hello {}'
 	name = request.args.get('name', 'World')
 	return template.format(name)
+
+@app.route('/distance')
+def distance():
+	from_string = request.args.get('from', None)
+	to_string = request.args.get('to', None)
+
+	if from_string is not None and to_string is not None:
+		print([float(i) for i in from_string.split(',')])
+		from_location = [float(item) for item in from_string.split(',')]
+		to_location = [float(item) for item in to_string.split(',')]
+		return '{}'.format(haversine(from_location, to_location))
+	else:
+		return 'Try again by using URL parameters \'from\' and \'to\' like so: distance?from=53.3,4.4&to=51.4,3.6'
 
 if __name__ == '__main__':
 	app.run(debug=True)
